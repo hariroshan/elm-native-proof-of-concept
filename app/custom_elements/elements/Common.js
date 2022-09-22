1d61be70ab69870ef4fe2450cf8778110b58e904
hariroshan <hariroshanmail@gmail.com>
2022-09-22 15:50:11 +0530

    added deep assign

--- app/custom_elements/elements/Common.js
+++ app/custom_elements/elements/Common.js
@@ -1,10 +1,15 @@
 function setAttribute(key, value, object) {
   if (key.includes(".")) {
     const keys = key.split(".");
-    object[keys[0]][keys[1]] = value;
+    assign_deep(object, keys, 0, value);
   } else {
     object[key] = value;
   }
+}
+
+function assign_deep(object, keys, i, value) {
+  object[keys[i]] = (keys.length - 1 === i) ? value : assign_deep(object[keys[i]], keys, i + 1, value)
+  return object
 }
 
 export const init = (object, props) => {
